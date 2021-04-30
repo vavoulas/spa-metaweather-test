dadb5ff156740884c8705acaa3517a2350ad522a
vavoulas <vavoulasnikos@gmail.com>
2021-04-30 04:56:17 +0300

    second

--- src/components/Weathersearch.jsx
+++ src/components/Weathersearch.jsx
@@ -1,79 +1,100 @@
-import React , { useState }from 'react'
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather"
 
 function Weathersearch() {
-    const [loc, setLoc] = useState({location: ""});
-    const [locData, setLocData] = useState([])
-    
-    //const [weather, setWeather] = useState([]);
+  const [loc, setLoc] = useState({ location: "" });
+  const [locData, setLocData] = useState([]);
+  const [condition, setCondition] = useState(false);
 
-    // const API = "https://www.metaweather.com/api/";
+  //const [weather, setWeather] = useState([]);
 
-    async function locationData(e) {
-        e.preventDefault();
-        if (loc.location === "") {
-          alert("NO LOCATION");
-        } else {
-          const data = await fetch(
-            `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${loc.location}`
-          )
-            .then((res) => res.json())
-            .then((data) => data);
-    
-          setLocData(...locData, data);
-        }
-      }
+  // const API = "https://www.metaweather.com/api/";
 
-    const handleChange = (e) => {
-        let name = e.target.name;
-        let value = e.target.value;
-    
-        if (name === "location") {
-          setLoc({ ...loc, location: value });
-        }
-        
+  async function locationData(e) {
+    e.preventDefault();
+    if (loc.location === "") {
+      alert("NO LOCATION");
+    } else {
+      const data = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${loc.location}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
 
-    };
-    console.log(locData);
-    return (
-        <div className="weather container">
-            <h3 className="text-primary  text-center">Search Here</h3>
-            <br/>
+      setLocData(...locData, data);
+    }
+  }
 
-                <form className="mb-3" onSubmit={(e) => locationData(e)}>
-                    
-                    <label for="Location" className="form-label h4 text-muted">Location</label>
-                    <div className="d-flex justify-content-center">
-                        <input type="text" name="location" className="form-control" placeholder="Location"
-                        onChange={(e) => handleChange(e)}/>
-                    
-                    
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
 
-                        <button type="submit" className="btn btn-success py-2 mx-2 btn-lg" onClick={(e) => locationData(e)}> Search </button>
+    if (name === "location") {
+      setLoc({ ...loc, location: value });
+    }
+  };
 
-                    </div>
-                    
-                </form>
+    const weatherDisplay = (e) => {
+        setCondition(true);
+       
+    }; 
+  console.log(locData);
+  return (
+    <div className="weather container">
+      <h3 className="text-primary  text-center">Search Here</h3>
+      <br />
 
-                <div className="card" style={{width: "18rem"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Results</h5>
-                        <ul className="list-group ">
-                          
-                            {locData.map((place) => 
-                            
-                                 
-                              <button type="button" className="list-group-item list-group-item-action" key="place.woeid">{place.title}</button>
-                                  
-                                 
+      <form className="mb-3" onSubmit={(e) => locationData(e)}>
+        <label for="Location" className="form-label h4 text-muted">
+          Location
+        </label>
+        <div className="d-flex justify-content-center">
+          <input
+            type="text"
+            name="location"
+            className="form-control"
+            placeholder="Location"
+            onChange={(e) => handleChange(e)}
+          />
 
-                    ) } 
-                        </ul>
-                    </div>
-                </div> 
-
-                
+          <button
+            type="submit"
+            className="btn btn-success py-2 mx-2 btn-lg"
+            onClick={(e) => locationData(e)}>
+            {" "}
+            Search{" "}
+          </button>
         </div>
-    )
- }
+      </form>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Results</h5>
+          <ul className="list-group ">
+            {locData.map((place) => (
+              <div>
+                <button
+                type="button"
+                className="list-group-item list-group-item-action"
+                key="place.woeid"
+                onClick={(e)=> weatherDisplay(e)}>
+                {place.title}
+                </button>
+                <DisplayWeather city={place.title} id={place.woeid} condition={condition}/>
+              
+
+              </div>
+              
+
+              
+            ))}
+          </ul>
+          
+        </div>
+        
+      </div>
+    </div>
+  );
+}
 
 export default Weathersearch;
--- src/components/displayWeather.jsx
+++ src/components/displayWeather.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 
-function displayWeather(props) {
-    return (
-        <div>
+function DisplayWeather(props) {
+    const [weatherdata, setWeatherData] = useState([]);
+
+    
+        
+     useEffect(() => {
+        const getweather = async () => {
+            const data = await fetch(
+                `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${props.id}/`
+              )
+                .then((res) => res.json())
+                .then((data) => data);
+        
+                setWeatherData(...weatherdata, data)
+        }
+        getweather();
+        
+        
+         
+     }, [])
+     
+     console.log(weatherdata);
+         
+
+      
+        
             
-        </div>
-    )
-}
+            return props.condition ? (
+                <div className="jumbotron jumbotron-fluid">
+                    <React.Fragment>
+                        <div className="card">
+                            <h2 className="card-header">{props.city}</h2>
+                            <div className="card-body">
+                                <span className="card-title">{weatherdata.consolidated_weather[0].weather_state_name} <i></i></span>
+                                
+                                <button className="btn btn-danger">Close</button>
+                            </div>
+                        </div>
+                        
+        
+                    </React.Fragment>
+                        
+                       
+        
+                              
+                </div>
+            ) : null
+           
+         
+    
+    } 
 
-di
 
-export default displayWeather
+
+export default DisplayWeather;
 
